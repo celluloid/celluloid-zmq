53c45eb9a3201f1806841d9d7c1b3e37809303d4
//de <de@freed.network>
2015-05-13 22:12:49 -0700

    fixes to tests to suit updates

--- spec/celluloid/zmq/socket_spec.rb
+++ spec/celluloid/zmq/socket_spec.rb
@@ -3,7 +3,7 @@ require 'celluloid/rspec'
 RSpec.describe Celluloid::ZMQ::Socket, actor_system: :global do
 
   it "allows setting and getting ZMQ options on the socket" do
-    socket = Celluloid::ZMQ::RepSocket.new
+    socket = Celluloid::ZMQ::Socket::Rep.new
     socket.set(::ZMQ::IDENTITY, "Identity")
 
     identity = socket.get(::ZMQ::IDENTITY)
