b7b07c3637bff12acc7b341914d61a78fb0e6d1b
//de <de@freed.network>
2015-05-13 21:59:17 -0700

    add remaining types to deprecation

--- lib/celluloid/zmq/deprecate.rb
+++ lib/celluloid/zmq/deprecate.rb
@@ -2,8 +2,14 @@ module Celluloid
   module ZMQ
     ReadableSocket = Socket::Readable
     WritableSocket = Socket::Writable
-
     RepSocket = Socket::Rep
     ReqSocket = Socket::Req
+    DealerSocket = Socket::Dealer
+    RouterSocket = Socket::Router
+    PushSocket = Socket::Push
+    PullSocket = Socket::Pull
+    PubSocket = Socket::Pub
+    XPubSocket = Socket::XPub
+    SubSocket = Socket::Sub
   end
 end
