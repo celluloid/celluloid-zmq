2f0240340d5c1ec35f3e9d0d0faaef270cb5c497
Patrik Wenger <paddor@gmail.com>
2016-06-20 15:52:11 +0200

    readd accidentally deleted line

--- spec/celluloid/zmq/socket_spec.rb
+++ spec/celluloid/zmq/socket_spec.rb
@@ -1,5 +1,6 @@
 require "celluloid/rspec"
 
+RSpec.describe Celluloid::ZMQ::Socket, library: :ZMQ do
   it "allows setting and getting ZMQ identity on the socket" do
     socket = Celluloid::ZMQ::Socket::Rep.new
     socket.identity = "Identity"
